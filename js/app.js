92b5b9432fb07b634573e90d86aaffcbc7c3eb14
Jorge Sánchez Blanco <jorgesanchezblanco1989@gmail.com>
2020-06-07 13:56:42 +0200

    Addedd access control headers to server

--- js/app.js
+++ js/app.js
@@ -10,8 +10,14 @@ const employeesRouter = require('./routes/employees');
 const app = express();
 app.use(cors({
     credentials: true,
-    origin: ["*"],
+    origin: ["https://localhost:3000"],
 }));
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    next();
+});
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
--- ts/app.ts
+++ ts/app.ts
@@ -13,9 +13,18 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: ["*"],
+    origin: ["https://localhost:3000"],
   })
 );
+
+app.use(function (req: Request, res:Response, next:NextFunction) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  //res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 
 app.use(logger("dev"));
 app.use(bodyParser.json());
